682ca2d87056421e0786b8001360d196f1ae5e65
LorD ClockaN <davor@losinj.com>
2011-09-11 08:33:59 +0200

    Lowered SD slot voltage by 0,2V Lowered Wifi Voltage

--- arch/arm/mach-msm/board-saga-mmc.c
+++ arch/arm/mach-msm/board-saga-mmc.c
@@ -92,8 +92,8 @@ struct mmc_vdd_xlat {
 };
 
 static struct mmc_vdd_xlat mmc_vdd_table[] = {
-	{ MMC_VDD_28_29,	2850 },
-	{ MMC_VDD_29_30,	2900 },
+	{ MMC_VDD_28_29,	2650 },
+	{ MMC_VDD_29_30,	2700 },
 };
 
 static unsigned int sdslot_vdd = 0xffffffff;
@@ -227,7 +227,7 @@ static unsigned int saga_wifi_status(struct device *dev)
 }
 
 static struct mmc_platform_data saga_wifi_data = {
-	.ocr_mask		= MMC_VDD_28_29,
+	.ocr_mask		= MMC_VDD_25_26,
 	.status			= saga_wifi_status,
 	.register_status_notify	= saga_wifi_status_register,
 	.embedded_sdio		= &saga_wifi_emb_data,
